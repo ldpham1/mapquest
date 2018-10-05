9ab035128f679e262834540ed081745a6391c692
ldpham1 <34046045+ldpham1@users.noreply.github.com>
2018-10-04 18:27:19 -0700

    Add files via upload

---
+++ executable_module.py
@@ -0,0 +1,127 @@
+"""
+ICS 32 Project 3
+Author: Lillian Pham
+UCI_ID: 68168196
+"""
+
+"""
+This module reads the input and constructs the objects that will generate
+the program's output
+"""
+
+import open_mapquest_apis as api
+import output_implementations as output
+
+def read_locations() -> [str]:
+    """
+    Accepts the user's input for starting and ending locations
+    and returns a list of the locations
+    """
+    num_of_locations = int(input())
+    locations = []
+
+    for i in range(num_of_locations):
+        location = input()
+        locations.append(location)
+ 
+    return locations
+
+def read_requested_outputs() -> [str]:
+    """
+    Accepts the requested output from the user and returns a list of
+    those outputs
+    """
+    num_of_outputs = int(input())
+    outputs = []
+    
+    for i in range(num_of_outputs):
+        output = input().upper()
+        outputs.append(output)
+
+    return outputs
+
+def print_steps(result: dict) -> None:
+    """
+    Constructs an object by calling class STEPS from module
+    output_implementations.py and generates the output for STEPS
+    """
+    s = api.steps(result)
+    steps = output.STEPS(s)
+    steps.generate_output()
+
+def print_total_distance(result: dict) -> None:
+    """
+    Constructs an object by calling class TOTALDISTANCE from module
+    output_implementations.py and generates the output for TOTALDISTANCE
+    """
+    d = api.total_distance(result)
+    total_distance = output.TOTALDISTANCE(d)
+    total_distance.generate_output()
+
+def print_total_time(result: dict) -> None:
+    """
+    Constructs an object by calling class TOTALTIME from module
+    output_implementations.py and generates the output for TOTALTIME
+    """
+    t = api.total_time(result)
+    total_time = output.TOTALTIME(t)
+    total_time.generate_output()
+
+def print_latLng(result: dict) -> None:
+    """
+    Constructs an object by calling class LATLONG from module
+    output_implementations.py and generates the output for LATLONG
+    """
+    l = api.latLng(result)
+    latLng = output.LATLONG(l)
+    latLng.generate_output()
+
+def print_elevation(result: dict) -> None:
+    """
+    Constructs an object by calling class ELEVATION from module
+    output_implementations.py and generates the output for ELEVATION
+    """
+    e = api.elevation(result)
+    elevation = output.ELEVATION(e)
+    elevation.generate_output()
+    
+def print_requested_outputs(requested_outputs: list, result: dict) -> None:
+    """
+    Checks the output specified by the user and calls the appropriate
+    print function
+    """
+    for output in requested_outputs:
+        if output == "STEPS":
+            print_steps(result)
+        elif output == "TOTALDISTANCE":
+            print_total_distance(result)
+        elif output == "TOTALTIME":
+            print_total_time(result)
+        elif output == "LATLONG":
+            print_latLng(result)
+        elif output == "ELEVATION":
+            elevation_urls = api.build_elevation_url(result)
+            print("\nELEVATIONS")
+            for url in elevation_urls:
+                elevation_result = api.get_result(url)
+                print_elevation(elevation_result)
+
+def run_navigation_system():
+    """
+    This is the main function that runs the entire navigation system
+    """
+    try:
+        locations = read_locations()
+        requested_outputs = read_requested_outputs()
+        directions_url = api.build_directions_url(locations)
+        result = api.get_result(directions_url)
+        print_requested_outputs(requested_outputs, result)
+        
+    except KeyError:
+        print("\nNO ROUTE FOUND.")
+        
+    finally:
+        print("\nDirections Courtesy of MapQuest; Map Data Copyright OpenStreetMap Contributors.") 
+
+if __name__ == "__main__":
+    run_navigation_system()
---
+++ open_mapquest_apis.py
@@ -0,0 +1,122 @@
+"""
+ICS 32 Project 3
+Author: Lillian Pham
+UCI_ID: 68168196
+"""
+
+"""
+This module interacts with the Open MapQuest APIs
+    -Builds URLs, makes HTTP requests, parses JSON responses, etc
+"""
+
+import http.client
+
+import urllib.request
+import urllib.parse
+import urllib.error
+
+import json
+
+API_KEY = "b4nfJRNmg1fh1HZ2ZoiOTgyGIiiGFLmv"
+
+def build_directions_url(locations: [str]) -> str:
+    """
+    Takes in a list of the specified locations to build the URL
+    of the Open Directions API and returns a list of the URLs
+    """
+    base_url = "http://open.mapquestapi.com/directions/v2/route?"
+    query_parameters = [("key", API_KEY)]
+    
+    for i in range(len(locations[:-1])):
+        query_parameters.append(("from", locations[i]))
+        query_parameters.append(("to", locations[i+1]))
+        
+    return base_url + urllib.parse.urlencode(query_parameters)
+
+def build_elevation_url(result: dict) -> [str]:
+    """
+    Builds the URL of the Open Elevation API and returns a list of the URLs
+    """
+    base_url = "http://open.mapquestapi.com/elevation/v1/profile?"
+    latLng_list = latLng(result)
+    URLs = []
+    
+    for dic in latLng_list:
+        query_parameters = [
+            ("key", API_KEY), (("latLngCollection", str(dic["lat"]) + "," + str(dic["lng"]))),
+            ("unit", "f")
+            ]
+        URLs.append(base_url + urllib.parse.urlencode(query_parameters))
+        
+    return URLs
+
+def get_result(URL: str) -> dict:
+    """
+    Takes in a list of URLs and returns a Python dictionary that represents
+    the parsed JSON response
+    """
+    response = None
+    loaded_text = []
+    
+    try:
+        response = urllib.request.urlopen(URL)
+        json_text = response.read().decode(encoding = "utf-8")
+        return json.loads(json_text)
+    
+    except urllib.error.URLError:
+        print("\nMAPQUEST ERROR")
+        
+    finally:
+        if response != None:
+            response.close()
+
+def steps(result: dict) -> [str]:
+    """
+    Returns the steps needed to get to the destination as a list
+    """
+    steps = []
+    
+    for dic in result["route"]["legs"]:
+        for sub_dic in dic["maneuvers"]:
+            steps.append(sub_dic["narrative"])
+            
+    return steps
+
+def total_distance(result: dict) -> float:
+    """
+    Returns the total distance needed to travel to get to the destination
+    """
+    total_distance = 0
+    total_distance += result["route"]["distance"]
+    return round(total_distance)
+
+def total_time(result: dict) -> int:
+    """
+    Returns the total amount of time it takes to complete the trip
+    """
+    total_time = 0
+    total_time += result["route"]["time"] / 60
+    return round(total_time)
+    
+def latLng(result: dict) -> [dict]:
+    """
+    Returns a list of dictionaries. Each dictionary contains the latitude and
+    longitude of each location specified by the user
+    """
+    latLng_list = []
+    
+    for dic in result["route"]["locations"]:
+        latLng_list.append(dic["latLng"])
+        
+    return latLng_list
+
+def elevation(result: dict) -> [int]:
+    """
+    Returns a list of integers that represent the elevation of each location
+    """
+    elevations = []
+    
+    for dic in result["elevationProfile"]:
+        elevations.append(round(dic["height"]))
+        
+    return elevations
